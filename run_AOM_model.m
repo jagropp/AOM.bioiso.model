3a636bdb90e666e14f3be6dc3419dab1cb253a36
JonGroJG <jonathan.gropp@weizmann.ac.il>
2021-01-17 11:36:17 +0200

    annotate function

--- run_AOM_model.m
+++ run_AOM_model.m
@@ -1,8 +1,11 @@
 function [] = run_AOM_model()
 
+% Best fit model parameters obtained with fmincon (tables S5 and S6)
+
+% dGr allocation between reactions 1-4
 xe(1,1:4) = [0.01 0.1201 0.8699 0.2];
 xe(2,1:4) = [0.01 0.9800 0.0100 0.5];
-xe(1:2,5:10) = repmat([0.979 0.9847 0.8881 1 1 0.7089],2,1);
+% Kinetic fractionation factors
 xe(1:2,5:10) = repmat([0.979 0.9847 0.8881 1 1 0.7089],2,1);
 
 d13C_CH4 = zeros(2,12);
@@ -14,7 +17,7 @@ for i = 1:2
                             run_bioiso_model(xe(i,:),i);
 end
 
-load fmincon_exp_dat.mat
+% Load data from experiments
+load('fmincon_exp_dat.mat','hig_so4_dat','low_so4_dat')
 
-fig = [1 0];
 plot_AOM_results
